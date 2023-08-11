314faab76036ea31b667090c647d04a2010d2295
Paolo Vincent Julian <scottypipz@gmail.com>
2023-08-11 18:22:49 +0800

    fix // Ensure that the "To Top Fab" component remains above other components by adjusting its z-index

--- src/_components/common/to-top-fab.tsx
+++ src/_components/common/to-top-fab.tsx
@@ -15,7 +15,7 @@ const ToTopFab: FunctionComponent<ToTopFabProps> = (props) => {
   };
 
   return (
-    <div className='fixed right-4 bottom-4'>
+    <div className='fixed right-4 bottom-4 z-50'>
       <button
         className='h-12 w-12 rounded-full bg-slate-800 md:hover:bg-red-400 active:scale-95 md:active:bg-red-500 flex justify-center items-center'
         onClick={handleClick}
