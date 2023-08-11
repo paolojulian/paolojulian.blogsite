7c07c5a7c4edbe796177e9c1ce2b034ef6c9a192
Paolo Vincent Julian <scottypipz@gmail.com>
2023-08-11 18:29:46 +0800

    style // Add box shadow to "To Top Fab" component

--- src/_components/common/to-top-fab.tsx
+++ src/_components/common/to-top-fab.tsx
@@ -18,6 +18,9 @@ const ToTopFab: FunctionComponent<ToTopFabProps> = (props) => {
     <div className='fixed right-4 bottom-4 z-50'>
       <button
         className='h-12 w-12 rounded-full bg-slate-800 md:hover:bg-red-400 active:scale-95 md:active:bg-red-500 flex justify-center items-center'
+        style={{
+          boxShadow: '0 4px 4px rgba(0,0,0,0.5)'
+        }}
         onClick={handleClick}
       >
         <TurnUpArrowIcon className='w-6 h-6 stroke-main' />
