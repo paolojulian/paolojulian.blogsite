544b8cb7396e693158c04005c3b932b9b364f000
Paolo Vincent Julian <scottypipz@gmail.com>
2023-08-14 15:27:04 +0800

    chore // Changed stroke-linejoin to strokeLinejoin

--- src/app/components/application-ui/layouts/brand-sidebar-with-header/_components/icons/cross-icon.tsx
+++ src/app/components/application-ui/layouts/brand-sidebar-with-header/_components/icons/cross-icon.tsx
@@ -16,8 +16,8 @@ const CrossIcon: FunctionComponent<CrossIconProps> = (props) => {
       {...props}
     >
       <path
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeLinecap='round'
+        strokeLinejoin='round'
         d='M6 18L18 6M6 6l12 12'
       />
     </svg>
