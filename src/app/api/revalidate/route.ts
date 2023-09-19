3304b8deb87762f674b448c2474495df5a5e1de6
Paolo Vincent Julian <paolo.julian@accelo.com>
2023-09-19 10:12:32 +0800

    feat // Allow cors on api/revalidate path

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -6,22 +6,7 @@ export async function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get('secret')
   const tag = request.nextUrl.searchParams.get('tag')
 
-  if (secret !== 'sadfaceqwe123!') {
-    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
-  }
-
-  if (!tag) {
-    return NextResponse.json({ message: 'Missing tag param' }, { status: 400 })
-  }
-
-  revalidateTag(tag)
-
-  return NextResponse.json({ revalidated: true, now: Date.now() })
-}
-
-export async function GET(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get('secret')
-  const tag = request.nextUrl.searchParams.get('tag')
+  console.log('Testing')
 
   if (secret !== 'sadfaceqwe123!') {
     return NextResponse.json({ message: 'Invalid secret' }, { status: 401 })
@@ -33,5 +18,12 @@ export async function GET(request: NextRequest) {
 
   revalidateTag(tag)
 
-  return NextResponse.json({ revalidated: true, now: Date.now() })
+  return NextResponse.json({ revalidated: true, now: Date.now() }, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
+      'Access-Control-Allow-Headers': 'Content-Type, Accept',
+    }
+  })
 }
