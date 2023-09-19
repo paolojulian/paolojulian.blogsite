1ac866134d7183a3da206a3eb74d8e06fc8be315
Paolo Vincent Julian <paolo.julian@accelo.com>
2023-09-19 14:52:44 +0800

    feat // Add vercel.json

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,10 @@ const allowedOrigins = ['https://app.contentful.com', 'https://paolojulian.dev/'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  console.log('Middleware')
   if (process.env.NODE_ENV === 'production') {
     const origin = request.headers.get('origin');
+    console.log('origin', origin)
     if (origin && !allowedOrigins.includes(origin)) {
       return new NextResponse(`Bad origin ${origin}`, {
         status: 400,
@@ -18,7 +20,14 @@ export function middleware(request: NextRequest) {
     }
   }
   const response = NextResponse.next()
-  response.headers.append("Access-Control-Allow-Origin", "*")
+  response.headers.append('Access-Control-Allow-Credentials', "true")
+  response.headers.append('Access-Control-Allow-Origin', '*') // replace this your actual origin
+  response.headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT')
+  response.headers.append(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  console.log('Added CORS headers')
 
   return response
 }
