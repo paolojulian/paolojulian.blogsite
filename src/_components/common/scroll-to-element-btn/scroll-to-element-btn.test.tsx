13c70da7f699c1b9991a51fa350ed79d630fed2d
Paolo Vincent Julian <paolo.julian@accelo.com>
2023-09-26 09:25:33 +0800

    test // Add snapshot to ScrollToElementBtn

--- src/_components/common/scroll-to-element-btn/scroll-to-element-btn.test.tsx
+++ src/_components/common/scroll-to-element-btn/scroll-to-element-btn.test.tsx
@@ -9,6 +9,18 @@ describe('TESTING ScrollToElementButton Component', () => {
   describe('GIVEN the ScrollToElementButton component with target ID and button text', () => {
     const targetId = 'test-target';
     const buttonText = 'Scroll to Target';
+
+    describe('WHEN the component is rendered to snapshot', () => {
+      it('THEN it should match the snapshot', () => {
+        const { asFragment } = render(
+          <ScrollToElementButton targetId={targetId}>
+            {buttonText}
+          </ScrollToElementButton>
+        );
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
+
     describe('WHEN the component is rendered', () => {
       it('THEN it should render the button with the provided children', () => {
         const { getByText } = render(
