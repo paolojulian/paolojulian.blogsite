9f40a103e9959764b8e7bd9ac048a6e5913e9187
Paolo Vincent Julian <paolo.julian@accelo.com>
2023-11-29 22:56:22 +0800

    fix // Deploy error regarding localStorage

--- src/app/apps/pomodoro/_components/PomodoroTasks/PomodoroTasks.utils.ts
+++ src/app/apps/pomodoro/_components/PomodoroTasks/PomodoroTasks.utils.ts
@@ -4,6 +4,10 @@ import { Task } from "@/app/apps/pomodoro/_components/PomodoroTasks/PomodoroTask
 const LOCAL_STORAGE_TASK_KEY = 'tasks';
 
 export function getTasksFromLocalStorage() {
+  if (typeof window === 'undefined') {
+    return [];
+  }
+
   const tasks = localStorage.getItem(LOCAL_STORAGE_TASK_KEY);
   if (!tasks) {
     return [];
@@ -13,5 +17,9 @@ export function getTasksFromLocalStorage() {
 }
 
 export function setTasksToLocalStorage(tasks: Task[]) {
+  if (typeof window === 'undefined') {
+    return;
+  }
+
   localStorage.setItem(LOCAL_STORAGE_TASK_KEY, JSON.stringify(tasks));
 }
